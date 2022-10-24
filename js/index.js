217b1b1f49ac710fe0c5f2fbb5afc733fde8508b
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-10-24 14:41:23 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,33 @@
-//Write your Javascript code here
-console.log("Shoppinglist")
+let myElement = document.getElementById("1");
+
+let myList = document.getElementsByTagName("li");
+
+console.log(myElement);
+
+//console.log(myList);
+
+myElement.setAttribute("hidden", true);
+
+for (let i = 0; i < myList.length; i++)
+{
+    if (myList[i].getAttribute("class") == "unhealthy")
+    {
+        myList[i].setAttribute("class", "healthy");
+    }
+
+    console.log(myList[i])
+}
+
+myParentNode = myElement.parentNode;
+
+console.log(myParentNode);
+
+let myListElement = document.createElement("li");
+myListElement.setAttribute("id", "5");
+myListElement.setAttribute("class", "healthy");
+
+let myText = document.createTextNode("Text");
+
+myListElement.append(myText);
+
+myParentNode.appendChild(myListElement);
